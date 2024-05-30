e978f5509b95a7fa32e8fe16e714d07e16cee2c4
jp3pe <rlaworhkd430430@gmail.com>
2024-05-30 16:25:42 +0900

    Clear text area before opening a file in GUI

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def draw_gui():
     text_area = tk.Text(root)
     text_area.grid(row=0, column=0, columnspan=2)
     add_button(root, text_area, "Save", lambda: save_text(text_area.get("1.0", "end-1c")))
-    add_button(root, text_area, "Open", lambda: text_area.insert(tk.END, open_text()))
+    add_button(root, text_area, "Open", lambda: [text_area.delete("1.0", tk.END), text_area.insert(tk.END, open_text())])
     root.mainloop()
 
 draw_gui()
